5253326acb17fc33a478152242b5f841b63af1d2
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-07-04 16:33:43 -0300

    feat(render): add function signatures and documentation to header file

--- include/render.h
+++ include/render.h
@@ -7,4 +7,23 @@
 #ifndef URBAN_TRAFFIC_RENDER_H
 #define URBAN_TRAFFIC_RENDER_H
 
+#include <stddef.h>
+#include "map.h"
+
+typedef struct Render Render;
+
+typedef struct {
+    Render *render;
+} RenderArgs;
+
+void render_config(size_t tile_width, size_t tile_height);
+
+void render_load_asset(TileType tile_type, const char *file_name);
+
+Render *render_new(void);
+
+void render_destroy(Render *render);
+
+void *render_update(void *render);
+
 #endif //URBAN_TRAFFIC_RENDER_H
